0842b5d43dc448be70447a572bc40c1494008d25
neopolii <ck1419@ic.ac.uk>
2023-03-22 11:31:19 +0000

    fixed f11 Vac

--- f11_delta.m
+++ f11_delta.m
@@ -4,8 +4,8 @@ function out = f11_delta(x, Pcon, Xarm, R, Rarm, Vgrid_RE, Vgrid_IM, Vhvdc, Pgri
 
     eqn(1) = - (revac*reiac) - (imvac*imiac) + (vdc*idc) - Pcon;
     eqn(2) = vdc + (idc*R) - Vhvdc;
-    eqn(3) = revac - (Xarm*imiac) - (Rarm*reiac) - Vgrid_RE;
-    eqn(4) = imvac + (Xarm*reiac) - (Rarm*imiac) - Vgrid_IM;
+    eqn(3) = revac + (Xarm*imiac) - (Rarm*reiac) - Vgrid_RE;
+    eqn(4) = imvac - (Xarm*reiac) - (Rarm*imiac) - Vgrid_IM;
     eqn(5) = (Vgrid_RE*reiac) + (Vgrid_IM*imiac) - Pgrid;
     eqn(6) = (Vgrid_IM*reiac) - (Vgrid_RE*imiac) - Qgrid;
 
