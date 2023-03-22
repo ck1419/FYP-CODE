13b0bb9979f681c8396bf696101940ef235443af
neopolii <ck1419@ic.ac.uk>
2023-03-22 11:07:15 +0000

    fixed f11 Sgrid

--- f11_delta.m
+++ f11_delta.m
@@ -6,8 +6,8 @@ function out = f11_delta(x, Pcon, Xarm, R, Rarm, Vgrid_RE, Vgrid_IM, Vhvdc, Pgri
     eqn(2) = vdc + (idc*R) - Vhvdc;
     eqn(3) = revac - (Xarm*imiac) - (Rarm*reiac) - Vgrid_RE;
     eqn(4) = imvac + (Xarm*reiac) - (Rarm*imiac) - Vgrid_IM;
-    eqn(5) = (Vgrid_RE*reiac) - (Vgrid_IM*imiac) - Pgrid;
-    eqn(6) = (Vgrid_IM*reiac) + (Vgrid_RE*imiac) - Qgrid;
+    eqn(5) = (Vgrid_RE*reiac) + (Vgrid_IM*imiac) - Pgrid;
+    eqn(6) = (Vgrid_IM*reiac) - (Vgrid_RE*imiac) - Qgrid;
 
     temp = jacobian(eqn, state_variables);
     out = subs(temp, state_variables, transpose(x));
