355d9e134f57c2dfc6c354d7374c52541a7dc461
neopolii <ck1419@ic.ac.uk>
2023-06-06 01:37:46 +0100

    added sqrt(2) to halfbridge AC

--- Code/Functions/check_limit.m
+++ Code/Functions/check_limit.m
@@ -1,6 +1,6 @@
 %Checks for the converter's limitations
 function pass = check_limit(ac, dc, rated, is_halfbridge)
-    if (abs(ac)*sqrt(2) + abs(dc) >= rated) || (abs(ac) - abs(dc) < 0 && is_halfbridge)
+    if (abs(ac)*sqrt(2) + abs(dc) >= rated) || (abs(ac)*sqrt(2) - abs(dc) < 0 && is_halfbridge)
         pass = 1;
     else
         pass = 0;
