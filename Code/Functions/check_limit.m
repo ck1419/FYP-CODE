fa3114d15c0c2cb835afb69c432aa3c68fdba698
neopolii <ck1419@ic.ac.uk>
2023-06-08 16:04:03 +0100

    halfbridge working

--- Code/Functions/check_limit.m
+++ Code/Functions/check_limit.m
@@ -1,9 +1,9 @@
 %Checks for the converter's limitations
 %0 = Fail
-function pass = check_limit(ac, dc, rated_voltage)
-    if abs(ac)*sqrt(2) + abs(dc) >= rated_voltage
-        pass = 0;
-    else
+function pass = check_limit(ac, dc, rated, halfbridge)
+    if (abs(ac)*sqrt(2) + abs(dc) >= rated) %|| ((abs(dc) - abs(ac)*sqrt(2) < 0) && halfbridge)
         pass = 1;
+    else
+        pass = 0;
     end
 end
