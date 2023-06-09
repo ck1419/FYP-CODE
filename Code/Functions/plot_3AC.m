4473ea25f780e30044cf00cb50d73cfb71777f10
neopolii <ck1419@ic.ac.uk>
2023-06-09 11:54:34 +0100

    updated 3phase legend units

--- Code/Functions/plot_3AC.m
+++ Code/Functions/plot_3AC.m
@@ -7,15 +7,15 @@ function plot_3AC(Vac_a, Iac_a, Vac_b, Iac_b, Vac_c, Iac_c, heading, pos, msg)
     axis equal
 
     %Plots points as arrows with a legend
-    quiver(0, 0, real(Vac_a)/100, imag(Vac_a)/100)
+    quiver(0, 0, real(Vac_a)/1000, imag(Vac_a)/1000)
     quiver(0, 0, real(Iac_a), imag(Iac_a))
-    quiver(0, 0, real(Vac_b)/100, imag(Vac_b)/100)
+    quiver(0, 0, real(Vac_b)/1000, imag(Vac_b)/1000)
     quiver(0, 0, real(Iac_b), imag(Iac_b))
-    quiver(0, 0, real(Vac_c)/100, imag(Vac_c)/100)
+    quiver(0, 0, real(Vac_c)/1000, imag(Vac_c)/1000)
     quiver(0, 0, real(Iac_c), imag(Iac_c))
     xlabel('Real')
     ylabel('Imaginary')
-    legend('[A] Vac/100 (V)', '[A] Iac (A)', '[B] Vac/100 (V)', '[B] Iac (A)', '[C] Vac/100 (V)', '[C] Iac (A)')
+    legend('[A] Vac/100 (kV)', '[A] Iac (A)', '[B] Vac (kV)', '[B] Iac (A)', '[C] Vac (kV)', '[C] Iac (A)')
     title(heading)
 
     %Adds text box with custom message at a custom position
